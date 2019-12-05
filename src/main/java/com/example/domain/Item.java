f39b81334d41ff668529d11c2620bf5c00e20de8
yosukeYamada <yamada_141@yahoo.co.jp>
2019-12-05 11:08:02 +0900

    Userドメイン、Toppingドメイン、Itemドメインを作成

---
+++ src/main/java/com/example/domain/Item.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+public class Item {
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private Integer priceM;
+	private Integer priceL;
+	private String imagePath;
+	private Boolean deleted;
+	private List<topping> toppingList;
+
+}
